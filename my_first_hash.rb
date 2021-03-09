888d9d17efdfcf34a27c2dee3498617c438a2594
meaghan jennings <meaghanljennings@gmail.com>
2021-03-09 23:22:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,32 +1,37 @@
 def my_hash
-  your_hash = {"key1" => "value1", "key1" => "value2"}
+  my_hash = {"lunch" => "chickpea sammie", "snack" => "kettle chips"}
 end
 
 def shipping_manifest
   the_manifest = {
-    "whale bone corsets" => 5,
-    "porcelain vases" => 2,
-    "oil paintings" => 3
-}
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                  }
 end
 
 def retrieval
   shipping_manifest = {
-    "whale bone corsets" => 5,
-    "porcelain vases" => 2,
-    "oil paintings" => 3
-}
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+   # Look up the value of the "oil paintings" key in the shipping_manifest hash below                
 shipping_manifest["oil paintings"]
-end 
+end
 
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-}
-  shipping_manifest["muskets"] = 2
+                 }
+  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] =  2
+
+  # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
 
-shipping_manifest
-end
+  # return the shipping_manifest hash below
+  shipping_manifest
+end 

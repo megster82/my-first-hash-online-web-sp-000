b441a4152c3e31b2fea57b3e1cff1a13318d0e87
meaghan jennings <meaghanljennings@gmail.com>
2021-03-09 23:19:28 +0000

    Automatically backed up by Learn

---
+++ my_first_hash.rb
@@ -0,0 +1,31 @@
+def my_hash
+  your_hash = {"key1" => "value1", "key1" => "value2"}
+end
+
+def the_shipping_manifest
+  shipping_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+}
+
+def retrieval
+  shipping_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+}
+shipping_manifest["oil paintings"]
+end 
+
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+}
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gunpowder"] = 4
+
+return shipping_manifest
+end
